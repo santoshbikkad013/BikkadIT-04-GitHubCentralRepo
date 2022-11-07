593ce6d1b4ce10f6ef98ac1150cecf41921a324a
santoshbikkad013 <santoshbikkad013@gmail.com>
2022-11-07 10:14:13 +0530

    Project added to central repo

---
+++ FirstGitApp/src/main/java/com/BikkadIT/FirstGitApp/FirstGitAppApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.FirstGitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGitAppApplication.class, args);
+	}
+
+}
---
+++ FirstGitApp/src/test/java/com/BikkadIT/FirstGitApp/FirstGitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.FirstGitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
